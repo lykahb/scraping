c4e636db874a7341cb87d5a2d2ee9113cfeaaae8
Boris Lykah <blykah@stilo.com>
2015-03-21 11:24:21 -0400

    category definition

--- scraping/items.py
+++ scraping/items.py
@@ -8,7 +8,8 @@
 import scrapy
 
 
-class ScrapingItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class CategoryItem(scrapy.Item):
+    department = scrapy.Field()
+    category_id = scrapy.Field()
+    name = scrapy.Field()
+    link = scrapy.Field()
