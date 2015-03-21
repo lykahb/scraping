9fedc7a7e17eb36a5ed75f3385f3b9c8de3f3525
Boris Lykah <blykah@stilo.com>
2015-03-21 11:53:09 -0400

    scrap categories

--- scraping/spiders/visions.py
+++ scraping/spiders/visions.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
+from urlparse import urljoin
+
 import scrapy
+from scrapy.utils.response import get_base_url
+
+from scraping.items import CategoryItem
 
 
 class VisionsSpider(scrapy.Spider):
@@ -10,4 +15,12 @@ class VisionsSpider(scrapy.Spider):
     )
 
     def parse(self, response):
-        pass
+        for dep in response.css('#mastermenu-dropdown > li'):
+            dep_name = dep.xpath('a').css('::text').extract()
+            categories = dep.css('div.mastermenu-bigsub') or dep.xpath('a')
+
+            for cat in categories:
+                for link in cat.css('a[href^="/Catalogue/"]'):
+                    url = urljoin(get_base_url(response), link.xpath('@href').extract()[0])
+                    name = (link.css(".mastermenu-bigsub-title") or link).css('::text').extract()
+                    yield CategoryItem(department=dep_name, name=name, link=url)
